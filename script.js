fc841e7eb60e92e70bfe4e0b07839df9db66079c
kasisalikhan <146641723+kasisalikhan@users.noreply.github.com>
2023-10-24 21:12:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+
+ 
+   //Questions array
+   let questions = [
+    {
+         number:1,
+         question: "Which part of a flower is responsible for producing pollen?",
+         answer : "Stamen",
+         options : [
+                          "Petal",
+                          "Sepal",
+                          "Stamen",
+                          "Pistil"
+                        ]
+    
+    },
+    {
+     number:2,
+     question: "What is the primary function of petals in a flower?",
+     answer : "Attracting pollinators",
+     options : [
+                      "Photosynthesis",
+                      "Attracting pollinators",
+                      "Supporting the flower structure",
+                      "Absorbing water"
+                   ]
+     },
+    {
+         number:3,
+         question: "What is the rarest flower in the world?",
+         answer : "Ghost Orchid",
+         options : [
+          "Wolffia globosa",
+          "Ghost Orchid",
+          "Montsechia vidalii",
+          "Rafflesia arnoldii"
+                          ]
+    
+    },
+   {
+         number:4,
+         question: "What is the female reproductive organ of a flower called?",
+         answer : "Pistil",
+         options : [ 
+                          "Stamen",
+                          "Pistil",
+                          "Anther",
+                          "Filament "
+                          ]
+    
+    },
+    {
+         number:5,
+         question: " What is the largest flower in the world?",
+         answer : "Rafflesia arnoldii",
+         options : [
+                          "Wolffia globosa",
+                          "Ghost Orchid",
+                          "Montsechia vidalii",
+                          "Rafflesia arnoldii"
+                          ]
+    }
+  
+     ];    
+
+     
